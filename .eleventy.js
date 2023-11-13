9dab2d299e2e7a35cbaafec5b654332482d334d7
Grayson Campbell <grayson.campbell@papercut.com>
2023-11-13 11:34:02 +1100

    Feat: Added tailwind to work with both storybook and prod

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,9 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy('site/assets');
 	eleventyConfig.addPassthroughCopy('site/_cloudcannon');
+	eleventyConfig.addPassthroughCopy({
+		"component-library/shared/styles/tailwind.out.css": "site/assets/tailwind.css",
+	  });
 
 	return {
 		dir: {
