27e2f1c5339c3eaa5077abc21401f7b41cf90214
Grayson Campbell <grayson.campbell@papercut.com>
2024-05-30 05:32:22 +1000

    Feat: Completish homepage.

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
 const pluginBookshop = require("@bookshop/eleventy-bookshop");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
-
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
 	eleventyConfig.htmlTemplateEngine = "liquid";
-
+	// eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 	eleventyConfig.addPlugin(pluginBookshop({
 		bookshopLocations: ["component-library"],
 		pathPrefix: ""
 	}));
-
 	eleventyConfig.addPassthroughCopy('site/assets');
 	eleventyConfig.addPassthroughCopy('site/_cloudcannon');
 	eleventyConfig.addPassthroughCopy({
