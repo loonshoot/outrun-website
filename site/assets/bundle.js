9d0e6addc53e9192d2b1ab0f0dc1de40daba592f
Grayson Campbell <grayson.campbell@papercut.com>
2024-06-12 20:26:31 +1000

    Feat: Subscribe form added.

--- site/assets/bundle.js
+++ site/assets/bundle.js
@@ -65,3 +65,20 @@ subscribeButton.addEventListener('click', () => {
     console.log("Redirecting to /thank-you"); // Log the redirect
   }, 1000); 
 });
+const subscribeBannerButton = document.getElementById('subscribebanner_subscribeButton');
+const subscribeBanneremailInput = document.getElementById('subscribebanner_emailInput');
+
+subscribeBannerButton.addEventListener('click', () => {
+  const email = subscribeBanneremailInput.value;
+  console.log("Email:", email); // Log the email value
+
+  // Send the event to outrun.js
+  outrun.stream("form_submitted", { email: email });
+  console.log("Outrun event streamed"); // Log that the event was sent
+
+  // Redirect to /thank-you
+  setTimeout(() => { // Delay the redirect slightly
+    window.location.href = '/thank-you';
+    console.log("Redirecting to /thank-you"); // Log the redirect
+  }, 1000); 
+});
